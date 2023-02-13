a0e7e7e25337e17299961c0cf2825d9f470df97f
Seungyeol <marudev4905@gmail.com>
2023-02-14 03:17:09 +0900

    'common_guide-update'

--- js/common.js
+++ js/common.js
@@ -1,6 +1,47 @@
 (function(){
-    // select event
     
+    // filter event
+    const filterBtn = document.querySelector(".search_wrap .filter_open_btn");
+    const filterEl = document.querySelector(".filter_wrap");
+    let isFilter = false;
+    
+    const toggleFilterEvt = () => {
+        if(!isFilter){
+            filterEl.classList.add("show");
+            filterBtn.classList.add("active");
+            isFilter = true;
+        }else{
+            filterEl.classList.remove("show");
+            filterBtn.classList.remove("active");
+            for(let i = 0; i < document.querySelectorAll(".filter_group .filter_box").length; i++){
+                document.querySelectorAll(".filter_wrap .filter_box")[i].querySelector("ul").classList.remove("show");
+            }
+            isFilter = false;
+        }
+    }
+    
+    filterBtn.addEventListener("click", toggleFilterEvt);
+    
+    
+    // select event
+    const selectBtns = document.querySelectorAll(".select_wrap button");
+    
+    function clickSelectEvt(){
+        let thisTxt = this.innerText;
+        let thisSelect = this.parentNode.parentNode;
+        
+        thisSelect.classList.contains("show") ? 
+            thisSelect.classList.remove("show") :
+            thisSelect.classList.add("show");
+        
+        thisSelect.querySelector(".select_btn").innerText = thisTxt;
+        thisSelect.querySelector(".select_btn").classList.add("active");
+    }
+
+    selectBtns.forEach((selectBtn)=>{
+        selectBtn.addEventListener("click", clickSelectEvt);
+    });
+
     
     // multi input range event
     const inputLeft = document.getElementById("input-left");
@@ -17,10 +58,8 @@
         const _this = inputLeft;
         const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
         
-        // 교차되지 않게, 1을 빼준 건 완전히 겹치기보다는 어느 정도 간격을 남겨두기 위해.
         _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1000);
         
-        // input, thumb 같이 움직이도록
         const percent = ((_this.value - min) / (max - min)) * 100;
         thumbLeft.style.left = percent + "%";
         range.style.left = percent + "%";
@@ -32,10 +71,8 @@
         const _this = inputRight;
         const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
         
-        // 교차되지 않게, 1을 더해준 건 완전히 겹치기보다는 어느 정도 간격을 남겨두기 위해.
         _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1000);
         
-        // input, thumb 같이 움직이도록
         const percent = ((_this.value - min) / (max - min)) * 100;
         thumbRight.style.right = 100 - percent + "%";
         range.style.right = 100 - percent + "%";
