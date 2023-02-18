ecb7983488ac90d0c9517a55635a2721ec3217c7
Seungyeol <marudev4905@gmail.com>
2023-02-18 17:15:24 +0900

    'js_split'

--- js/common.js
+++ js/common.js
@@ -1,130 +1,160 @@
+
 (function(){
-    
-    // filter event
-    const filterBtn = document.querySelector(".search_wrap .filter_open_btn");
-    const filterEl = document.querySelector(".filter_wrap");
-    let isFilter = false;
-    
-    const toggleFilterEvt = () => {
-        if(!isFilter){
-            filterEl.classList.add("show");
-            filterBtn.classList.add("active");
-            isFilter = true;
-        }else{
-            filterEl.classList.remove("show");
-            filterBtn.classList.remove("active");
-            for(let i = 0; i < document.querySelectorAll(".filter_group .filter_box").length; i++){
-                document.querySelectorAll(".filter_wrap .filter_box")[i].querySelector("ul").classList.remove("show");
+    let commonJS = {
+        selectEvt:function(){
+            // select event
+            const selectBtns = document.querySelectorAll(".select_wrap button");
+            
+            function clickSelectEvt(){
+                let thisTxt = this.innerText;
+                let thisSelect = this.parentNode.parentNode;
+                
+                thisSelect.classList.contains("show") ? 
+                    thisSelect.classList.remove("show") :
+                    thisSelect.classList.add("show");
+                
+                thisSelect.querySelector(".select_btn").innerText = thisTxt;
+                thisSelect.querySelector(".select_btn").classList.add("active");
             }
-            isFilter = false;
+
+            selectBtns.forEach((selectBtn)=>{
+                selectBtn.addEventListener("click", clickSelectEvt);
+            });
+        },
+        accoEvt:function(){
+            function HamburgerEve(){
+                let btnMenu = document.querySelector(".btn_hamburger");
+                let gnb = document.querySelector(".gnb_mo");
+                let i;
+
+                btnMenu.addEventListener("click", function() {
+                    if (gnb.style.maxHeight){
+                        gnb.style.maxHeight = null;
+                    } else {
+                        gnb.style.maxHeight = gnb.scrollHeight + "px";
+                    } 
+                });
+            }
+            HamburgerEve();
+
+            function gnbEvt(){
+                let category = document.querySelectorAll(".gnb_mo > li > a");
+                let i;
+
+                for (i = 0; i < category.length; i++) {
+                category[i].addEventListener("click", function() {
+                    this.classList.toggle("active");
+                    let content = this.nextElementSibling;
+                    if (content.style.maxHeight){
+                        content.style.maxHeight = null;
+                    } else {
+                        content.style.maxHeight = content.scrollHeight + "px";
+                    } 
+                });
+                }
+            }
+            gnbEvt();
+        },
+        videoEvt:function(){
+            let item = document.querySelectorAll(".sec_search_result .item");
+
+            for(i = 0; i < item.length; i++){
+                item[i].addEventListener('mouseenter', function(){
+                    let thumbBox = this.children[0];
+                    let video = thumbBox.children[1];
+
+                    async function playVideo() {
+                        try {
+                            await video.play();
+                        } catch (err) {
+                            console(err);
+                        }
+                    }
+                    video.classList.add("on");
+                    playVideo();
+                });
+
+                item[i].addEventListener('mouseleave', function(){
+                    let thumbBox = this.children[0];
+                    let video = thumbBox.children[1];
+                    video.classList.remove("on");
+                    video.pause();
+
+                });
+            }
+        },
+        multiIptRangeEvt:function(){
+            // multi input range event
+            const inputLeft = document.getElementById("input-left");
+            const inputRight = document.getElementById("input-right");
+
+            const thumbLeft = document.querySelector(".slider > .thumb.left");
+            const thumbRight = document.querySelector(".slider > .thumb.right");
+            const range = document.querySelector(".slider > .range");
+            
+            let rangeMinTxt = document.querySelector(".middle .middle_bottom .min_txt");
+            let rangeMaxTxt = document.querySelector(".middle .middle_bottom .max_txt");
+
+            const setLeftValue = () => {
+                const _this = inputLeft;
+                const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
+                
+                _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1000);
+                
+                const percent = ((_this.value - min) / (max - min)) * 100;
+                thumbLeft.style.left = percent + "%";
+                range.style.left = percent + "%";
+
+                rangeMinTxt.innerHTML = _this.value;
+            };
+            
+            const setRightValue = () => {
+                const _this = inputRight;
+                const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
+                
+                _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1000);
+                
+                const percent = ((_this.value - min) / (max - min)) * 100;
+                thumbRight.style.right = 100 - percent + "%";
+                range.style.right = 100 - percent + "%";
+
+                rangeMaxTxt.innerHTML = _this.value;
+            };
+
+            inputLeft.addEventListener("input", setLeftValue);
+            inputRight.addEventListener("input", setRightValue);
+        },
+        filterEvt:function(){
+            // filter event
+            const filterBtn = document.querySelector(".search_wrap .filter_open_btn");
+            const filterEl = document.querySelector(".filter_wrap");
+            let isFilter = false;
+            
+            const toggleFilterEvt = () => {
+                if(!isFilter){
+                    filterEl.classList.add("show");
+                    filterBtn.classList.add("active");
+                    isFilter = true;
+                }else{
+                    filterEl.classList.remove("show");
+                    filterBtn.classList.remove("active");
+                    for(let i = 0; i < document.querySelectorAll(".filter_group .filter_box").length; i++){
+                        document.querySelectorAll(".filter_wrap .filter_box")[i].querySelector("ul").classList.remove("show");
+                    }
+                    isFilter = false;
+                }
+            }
+            
+            filterBtn.addEventListener("click", toggleFilterEvt);
+        },
+
+        init:function(){
+            this.accoEvt();
+            this.videoEvt();
+            this.filterEvt();
+            this.selectEvt();
+            this.multiIptRangeEvt();
         }
     }
-    
-    filterBtn.addEventListener("click", toggleFilterEvt);
-    
-    
-    // select event
-    const selectBtns = document.querySelectorAll(".select_wrap button");
-    
-    function clickSelectEvt(){
-        let thisTxt = this.innerText;
-        let thisSelect = this.parentNode.parentNode;
-        
-        thisSelect.classList.contains("show") ? 
-            thisSelect.classList.remove("show") :
-            thisSelect.classList.add("show");
-        
-        thisSelect.querySelector(".select_btn").innerText = thisTxt;
-        thisSelect.querySelector(".select_btn").classList.add("active");
-    }
-
-    selectBtns.forEach((selectBtn)=>{
-        selectBtn.addEventListener("click", clickSelectEvt);
-    });
-
-    
-    // multi input range event
-    const inputLeft = document.getElementById("input-left");
-    const inputRight = document.getElementById("input-right");
-
-    const thumbLeft = document.querySelector(".slider > .thumb.left");
-    const thumbRight = document.querySelector(".slider > .thumb.right");
-    const range = document.querySelector(".slider > .range");
-    
-    let rangeMinTxt = document.querySelector(".middle .middle_bottom .min_txt");
-    let rangeMaxTxt = document.querySelector(".middle .middle_bottom .max_txt");
-
-    const setLeftValue = () => {
-        const _this = inputLeft;
-        const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
-        
-        _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1000);
-        
-        const percent = ((_this.value - min) / (max - min)) * 100;
-        thumbLeft.style.left = percent + "%";
-        range.style.left = percent + "%";
-
-        rangeMinTxt.innerHTML = _this.value;
-    };
-    
-    const setRightValue = () => {
-        const _this = inputRight;
-        const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
-        
-        _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1000);
-        
-        const percent = ((_this.value - min) / (max - min)) * 100;
-        thumbRight.style.right = 100 - percent + "%";
-        range.style.right = 100 - percent + "%";
-
-        rangeMaxTxt.innerHTML = _this.value;
-    };
-
-    inputLeft.addEventListener("input", setLeftValue);
-    inputRight.addEventListener("input", setRightValue);
-
-
-    // input number event
-    const numIptUpBtn = document.querySelector(".num_ipt_up_btn");
-    const numIptDownBtn = document.querySelector(".num_ipt_down_btn");
-
-    function clickNumIptUp(){
-        this.parentNode.querySelector("[type=number]").stepUp();
-    }
-    function clickNumIptDown(){
-        this.parentNode.querySelector("[type=number]").stepDown();
-    }
-
-    numIptUpBtn.addEventListener("click",clickNumIptUp);
-    numIptDownBtn.addEventListener("click",clickNumIptDown);
-    
-
-    // input file event
-    const fileIpts = document.querySelectorAll("input[type=file]");
-
-    function changeFileIpt(){
-        let fileList = [];
-
-        for(let i = 0; i < this.files.length; i++){
-            fileList.push(this.files[i].name);
-        }
-        
-        fileList.forEach((file)=>{
-            let li = document.createElement("li");
-            let span = document.createElement("span");
-            span.setAttribute("class","file_name");
-            span.innerText = file;
-            let button = document.createElement("button");
-            button.setAttribute("class","file_remove_btn ico_gray_close");
-            li.appendChild(span);
-            li.appendChild(button);
-
-            this.parentNode.querySelector(".file_list").appendChild(li);
-        })
-    }
-
-    fileIpts.forEach((fileIpt)=>{
-        fileIpt.addEventListener("change", changeFileIpt);
-    });
-
+    commonJS.init();
 })();
--- js/main.js
+++ js/main.js
@@ -1,74 +1,9 @@
 
 (function(){
     let mainJS = {
-        accoEvt:function(){
-            function HamburgerEve(){
-                let btnMenu = document.querySelector(".btn_hamburger");
-                let gnb = document.querySelector(".gnb_mo");
-                let i;
 
-                btnMenu.addEventListener("click", function() {
-                    if (gnb.style.maxHeight){
-                        gnb.style.maxHeight = null;
-                    } else {
-                        gnb.style.maxHeight = gnb.scrollHeight + "px";
-                    } 
-                });
-            }
-            HamburgerEve();
-
-            function gnbEvt(){
-                let category = document.querySelectorAll(".gnb_mo > li > a");
-                let i;
-
-                for (i = 0; i < category.length; i++) {
-                category[i].addEventListener("click", function() {
-                    this.classList.toggle("active");
-                    let content = this.nextElementSibling;
-                    if (content.style.maxHeight){
-                        content.style.maxHeight = null;
-                    } else {
-                        content.style.maxHeight = content.scrollHeight + "px";
-                    } 
-                });
-                }
-            }
-            gnbEvt();
-        },
-        videoEvt:function(){
-            let item = document.querySelectorAll(".sec_search_result .item");
-
-            for(i = 0; i < item.length; i++){
-                item[i].addEventListener('mouseenter', function(){
-                    let thumbBox = this.children[0];
-                    let video = thumbBox.children[1];
-
-                    async function playVideo() {
-                        try {
-                            await video.play();
-                        } catch (err) {
-                            console(err);
-                        }
-                    }
-                    video.classList.add("on");
-                    playVideo();
-                });
-
-                item[i].addEventListener('mouseleave', function(){
-                    let thumbBox = this.children[0];
-                    let video = thumbBox.children[1];
-                    video.classList.remove("on");
-                    video.pause();
-
-                });
-            }
-            
-
-            
-        },
         init:function(){
-            this.accoEvt();
-            this.videoEvt();
+
         }
     }
     mainJS.init();
