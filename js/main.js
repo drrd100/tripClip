14acc2cf6674bb69996c08fc4354937c6192e5b5
Seungyeol <marudev4905@gmail.com>
2023-02-18 18:06:48 +0900

    'main_ing'

--- js/main.js
+++ js/main.js
@@ -2,8 +2,48 @@
 (function(){
     let mainJS = {
 
-        init:function(){
+        swiperEvt:function(){
+            // new Swiper('.swiper-container', {
+            //     speed: 3500,
+            //     slidesPerView: 'auto',
+            //     loop: true,
+            //     autoplay:{
+            //         delay: 0,
+            //         disableOnInteraction: true,
+            //     },
+            // });
 
+            // let videoSwiper = document.querySelector(".slide_wrap .swiper-container").swiper;
+
+
+
+            // document.querySelector('.swiper-container').addEventListener("mouseenter", function(){
+            //     console.log(videoSwiper);
+            //     videoSwiper.autoplay.stop();
+            // })
+            // document.querySelector('.swiper-container').addEventListener("mouseleave", function(){
+            //     console.log(videoSwiper)
+            //     videoSwiper.autoplay.start();
+            // })
+            var mySwiper = new Swiper('.swiper-container',{
+                pagination: '.pagination',
+                loop:true,
+                autoplay: 1000,
+                paginationClickable: true
+              })
+              
+              document.querySelector('.swiper-container').addEventListener('mouseenter', function(e){
+                console.log('stop autoplay');
+                mySwiper.stopAutoplay();
+              })
+              document.querySelector('.swiper-container').addEventListener('mouseleave', function(e){
+                console.log('start autoplay');
+                mySwiper.startAutoplay();
+              })
+        },
+
+        init:function(){
+            this.swiperEvt();
         }
     }
     mainJS.init();
