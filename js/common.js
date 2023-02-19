cac2a64ba55757f318d8bd035301cc9f40e34423
hongyeol <drrd100@naver.com>
2023-02-20 00:25:06 +0900

    merge sy modify

--- js/common.js
+++ js/common.js
@@ -72,16 +72,8 @@
             gnbEvt();
         },
         videoEvt:function(){
-<<<<<<< HEAD
             let item = document.querySelectorAll("#video_list .item");
             
-=======
-            let item = document.querySelectorAll(".video_list .item");
-            // function loader(){
-            //     curItem.classList.remove("loader");
-            //     playVideo();
-            // }
->>>>>>> sy
             for(i = 0; i < item.length; i++){
                 item[i].addEventListener('mouseenter', function(){
                     let thumbBox = this.children[0];
